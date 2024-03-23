32c5c2ba289fe5c6824e9889b740270a569953c5
pfuchter <p.fuchter@bristol.ac.uk>
2024-03-23 19:39:22 +0000

    added empty3! to act on vector of lines, deletes those lines in ax

--- src/useful_functions.jl
+++ src/useful_functions.jl
@@ -1,5 +1,5 @@
 """
-`empty3!(ax)`
+`empty3!(ax,types)`
 
 Removes all objects from axis `ax` that have type contained in vector of types `types`
 """
@@ -10,9 +10,24 @@ function empty3!(ax,types)
             delete!(ax.scene,ax.scene.plots[i+c])            
             c = c - 1
         end
-        
     end
 end
+
+"""
+`empty3!(ax,)`
+
+Removes all lines in vector `lines_to_delete` from axis `ax`
+"""
+function empty3!(ax,lines_to_delete::Vector{Lines{Tuple{Vector{Point{3,Float32}}}}})
+    c = 0
+    for i = 1:length(ax.scene.plots)
+        if ax.scene.plots[i+c] in lines_to_delete
+            delete!(ax.scene,ax.scene.plots[i+c])            
+            c = c - 1
+        end
+    end
+end
+
 
 mean(x) = sum(x) / length(x)
 norm(x) = sqrt(sum(x.^2))
